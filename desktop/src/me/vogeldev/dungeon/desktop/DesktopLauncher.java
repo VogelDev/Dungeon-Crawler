ba24c9027f4c123bc0e150cf5378abd6e613f7f8
VogelDev <rvogel@mc3.edu>
2016-01-10 20:28:21 -0500

    LibGDX Project creation
    Created LibGDX project.

---
+++ desktop/src/me/vogeldev/dungeon/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package me.vogeldev.dungeon.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import me.vogeldev.dungeon.MainActivity;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new MainActivity(), config);
+	}
+}
