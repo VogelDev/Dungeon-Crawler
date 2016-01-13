79652cd0a98c2f56fa656cecdbbbf1b4ce083b1e
Vogel <robtvogel@gmail.com>
2016-01-13 17:59:27 -0500

    Test
    test

--- core/src/me/vogeldev/dungeon/MainActivity.java
+++ core/src/me/vogeldev/dungeon/MainActivity.java
@@ -39,8 +39,6 @@ public class MainActivity extends ApplicationAdapter {
 	@Override
 	public void create () {
 		batch = new SpriteBatch();
-
-		//test comment
 		
 		playerPos = new Vector2(0,0);
 		screenWidth = Gdx.graphics.getWidth();
